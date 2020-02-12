8701ded7963051d5c16fd00e0dc5e4fb4665a517
ccurnuck <calvin.curnuck@gmail.com>
2020-02-12 09:12:26 -0500

    Added Auth & Protected Routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,13 +3,20 @@ import { Route } from "react-router-dom";
 import LoginFormContainer from "./sessionFormComponents/login_form_container";
 import SignupFormContainer from "./sessionFormComponents/signup_form_container";
 import HeaderContainer from "./header/header_container";
+import { ProtectedRoute, AuthRoute } from "../utils/route_util";
 
 const App = () => (
     <div>
-        <HeaderContainer />
-        <Route path="/login" component={LoginFormContainer} />
-        <Route path="/signup" component={SignupFormContainer} />
+        <Route exact path="/" component={HeaderContainer} />
+        <ProtectedRoute path="/feed" component={HeaderContainer} />
+        <AuthRoute exact path="/login" component={LoginFormContainer} />
+        <AuthRoute exact path="/signup" component={SignupFormContainer} />
     </div>
   );
   
   export default App;
+
+//   <Route path="/" component={NavBarContainer} />
+//     <Route exact path="/" component={Home} />
+//     <ProtectedRoute path="/chirps" component={ChirpIndexContainer} />
+//     <AuthRoute path="/signup" component={SignupContainer} />
---
+++ frontend/utils/route_util.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { connect} from "react-redux";
+import { Redirect, Route, withRouter } from "react-router-dom";
+
+const Auth = ({ component: Component, path, loggedIn, exact }) => (
+    <Route
+      path={path}
+      exact={exact}
+      render={props =>
+        !loggedIn ? <Component {...props} /> : <Redirect to="/feed" />
+      }
+    />
+  );
+  
+  const mapStateToProps = state => {
+    return { loggedIn: Boolean(state.session.currentUserId) };
+  };
+  
+  export const AuthRoute = withRouter(
+    connect(mapStateToProps, null)(Auth)
+  );
+
+  const Protected = ({ loggedIn, path, component: Component }) => (
+    <Route
+        path={path}
+        render={props => (
+            loggedIn ? <Component {...props} /> : <Redirect to="/signup" />
+        )}
+    />
+);
+
+export const ProtectedRoute = withRouter(
+    connect(mapStateToProps)(Protected)
+);
