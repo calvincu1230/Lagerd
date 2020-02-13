854a809af22b656694461c75ef2ccab5df55497c
ccurnuck <calvin.curnuck@gmail.com>
2020-02-13 15:56:27 -0500

    Fixed Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Beer.destroy_all!
-Brewery.destroy_all!
-User.destroy_all!
+Beer.delete_all
+Brewery.delete_all
+User.delete_all
 
 # User Seeds
 
