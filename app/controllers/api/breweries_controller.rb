03c82372fa8a65086be869624cea56fd8536a082
ccurnuck <calvin.curnuck@gmail.com>
2020-03-17 11:11:44 -0400

    Minor CSS and Brewery Beer Assoc

--- app/controllers/api/breweries_controller.rb
+++ app/controllers/api/breweries_controller.rb
@@ -1,12 +1,17 @@
 class Api::BreweriesController < ApplicationController
 
   def index
-    @breweries = Brewery.includes(:beers).with_attached_photo
+    @breweries = Brewery.with_attached_photo
     render :index
   end
 
   def show
     @brewery = Brewery.includes(:beers).with_attached_photo.find(params[:id])
     render :show
+  end
+
+  def beers 
+    @beers = Brewery.find(params[:id]).beers.with_attached_photo
+    render "/api/beers/index.json.jbuilder"
   end
 end
