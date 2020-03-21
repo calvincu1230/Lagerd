3eb1e226b863899bea8b4d62480310a775a66d87
ccurnuck <calvin.curnuck@gmail.com>
2020-03-21 18:56:19 -0400

    Improved error line spacing and ordering of beer errors

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -16,7 +16,7 @@
 class Beer < ApplicationRecord
 
   validates :name, uniqueness: true
-  validates :name, :brewery_id, :style, :abv, :ibu, :description, presence: true
+  validates :name, :brewery_id, :abv, :ibu, :style, :description, presence: true
 
   has_many :checkins, dependent: :destroy
   # has_many :posters,
