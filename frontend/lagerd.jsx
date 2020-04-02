921280a7240658839335e9c840d20226a67d553d
ccurnuck <calvin.curnuck@gmail.com>
2020-04-02 11:40:53 -0400

    Configured store for production

--- frontend/lagerd.jsx
+++ frontend/lagerd.jsx
@@ -5,23 +5,23 @@ import configureStore from "./store/store";
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    // const store = configureStore();
+  // const store = configureStore();
     
-    let store;
-    if (window.currentUser) {
-        const preloadedState = {
-            entities: {
-                users: { [window.currentUser.id]: window.currentUser }
-            },
-            session: { currentUserId: window.currentUser.id }
-        };
-        store = configureStore(preloadedState);
-        delete window.currentUser;
-    } else {
-        store = configureStore();
-    }
-    window.store = store;
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { currentUserId: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  window.store = store;
 
     const root = document.getElementById("root");
-    ReactDOM.render(<Root store={store} />, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
