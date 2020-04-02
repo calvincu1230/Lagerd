68c7896387728c531fa201582b414c8684bd9b0b
ccurnuck <calvin.curnuck@gmail.com>
2020-04-02 13:08:59 -0400

    Fixed Typo

--- frontend/actions/toast_actions.js
+++ frontend/actions/toast_actions.js
@@ -2,7 +2,7 @@ import * as ToastAPI from "../utils/toast_api_util";
 
 export const REMOVE_TOAST = "REMOVE_TOAST";
 export const RECEIVE_TOAST = "RECEIVE_TOAST";
-export const RECEIVE_ALL_TOASTs = "RECEIVE_ALL_TOASTs";
+export const RECEIVE_ALL_TOASTS = "RECEIVE_ALL_TOASTS";
 export const RECEIVE_TOAST_ERRORS = "RECEIVE_TOAST_ERRORS";
 
 const receiveToast = toast => {
@@ -12,7 +12,7 @@ const receiveToast = toast => {
   };
 };
 
-const receiveAllToast = () => {
+const receiveAllToast = (toasts) => {
   return {
     type: RECEIVE_ALL_TOASTS,
     toasts
@@ -35,18 +35,18 @@ const receieveToastErrors = errors => {
 
 export const fetchAllToasts = () => dispatch => {
   ToastAPI.fetchAllToasts()
-    .then(toasts => dispatch(receiveAllToast(toasts)))
-    .catch(errors => dispatch(receieveToastErrors(errors)));
+    .then(toasts => dispatch(receiveAllToast(toasts)),
+    errors => dispatch(receieveToastErrors(errors)));
 };
 
 export const createToast = toast => dispatch => {
   ToastAPI.createToast(toast)
-    .then(toast => dispatch(receiveToast(toast)))
-    .catch(errors => dispatch(receieveToastErrors(errors)));
+    .then(toast => dispatch(receiveToast(toast)),
+    errors => dispatch(receieveToastErrors(errors)));
 };
 
 export const deleteToast = toastId => dispatch => {
   ToastAPI.deleteToast(toastId)
-    .then(toastId => dispatch(removeToast(toastId)))
-    .catch(errors => dispatch(receieveToastErrors(errors)));
+    .then(toastId => dispatch(removeToast(toastId)),
+    errors => dispatch(receieveToastErrors(errors)));
 };
