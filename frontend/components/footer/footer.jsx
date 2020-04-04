1bdf382df4a2ba2c0653a2b4b83567b3775f5f54
ccurnuck <calvin.curnuck@gmail.com>
2020-04-04 17:23:39 -0400

    Added social logos/links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,12 +5,23 @@ export default () => {
     return (
         <footer className="splash-footer">
           <div className="footer-main">
-            <div className="foot-name">
-              Calvin Curnuck
+            <div className="foot-name foot-link">
+              <a href="https://www.calvincurnuck.com">Calvin Curnuck</a>
             </div>
             <ul className="link-list">
-            <li className="github-link foot-link"><a href="https://github.com/calvincu1230/Lagerd">Github</a></li>
-            <li className="linkedin-link foot-link"><a href="https://www.linkedin.com/in/calvin-curnuck-7139a7a8/">LinkedIn</a></li>
+
+              <li className="social-link foot-link">
+                <a href="https://github.com/calvincu1230/Lagerd"><i className="fab fa-github"></i></a>
+              </li>
+
+              <li className="social-link foot-link">
+                <a href="https://www.linkedin.com/in/calvin-curnuck-7139a7a8/"><i className="fab fa-linkedin"></i></a>
+              
+              </li>
+              <li className="social-link foot-link">
+                <a href="https://angel.co/u/calvin-curnuck"><i class="fab fa-angellist"></i></a>
+              </li>
+
             </ul>
           </div>
         </footer>
