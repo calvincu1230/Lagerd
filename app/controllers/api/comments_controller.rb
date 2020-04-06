856b7ed9e83dd22257549469a83c753373e145e6
ccurnuck <calvin.curnuck@gmail.com>
2020-04-06 15:06:38 -0400

    Added comments controller and routes

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,37 @@
+class Api::CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render :show
+    else
+      render json: @comment.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment && @comment.update_attributes(comment_params)
+      render :show
+    elsif !@comment
+      render json: ["Could not find comment."], status: 400
+    else
+      render json: @comment.errors.full_messages, status: 401
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment && @comment.destroy
+      render :show
+    else
+      render json: ["Could not find comment."], status: 404
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :checkin_id, :author_id)
+  end
+end
