f3e2ad760f2c3a144b57c93d632b342973718863
ccurnuck <calvin.curnuck@gmail.com>
2020-04-06 13:47:24 -0400

    Added comment errors reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,12 +1,14 @@
-import sessionErrorsReducer from "./session_errors_reducer";
 import { combineReducers } from "redux";
+import sessionErrorsReducer from "./session_errors_reducer";
 import beerErrorsReducer from "./beer_errors_reducer";
 import checkinErrorsReducer from "./checkin_errors_reducer";
+import commentErrorsReducer from "./comment_errors_reducer";
 
 const errorsReducer = combineReducers({
     session:  sessionErrorsReducer,
     beer: beerErrorsReducer,
-    checkin: checkinErrorsReducer
+    checkin: checkinErrorsReducer,
+    comment: commentErrorsReducer
 });
 
 export default errorsReducer;
