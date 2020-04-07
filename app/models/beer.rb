3960ef5c97aedeff1ab2bf430a68540a99514e0a
ccurnuck <calvin.curnuck@gmail.com>
2020-04-07 12:41:50 -0400

    Updated Assocs and queries

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -21,9 +21,11 @@ class Beer < ApplicationRecord
   has_many :checkins, dependent: :destroy
   has_many :toasts, through: :checkins
   has_many :comments, through: :checkins
-  # has_many :posters,
-  #   through :checkins,
-  #   source 
+
+  has_many :checkin_authors, -> { distinct },
+    through: :checkins,
+    source: :author
+
   belongs_to :brewery
   has_one_attached :photo
 
@@ -33,23 +35,23 @@ class Beer < ApplicationRecord
     self.photo.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default_beer.png')), filename: 'default_beer.png') unless self.photo.attached?
   end
 
-  def uniq_users
+  def average_rating # average of ratings counting zeros 
     self.checkins
-      .group(:author_id)
-      .count
+      .average(:rating)
+      # .where("rating >= ?", 0) # in case I change table to str if no rating given
+  end
+
+  def uniq_users # counts num of unique users that have checked in a beer by brewery
+    self.checkin_authors.size
+  end
+
+  def checkins_this_month # num of checkins in current calendar month
+    dt = DateTime.now
+    bom = dt.beginning_of_month
+    eom = dt.end_of_month
+    self.checkins
+      .where("checkins.created_at >= ? and checkins.created_at <= ?", bom, eom)
       .size
   end
-
-  def average_rating
-    sum = 0
-    self.checkins.each do |checkin|
-      sum += checkin.rating
-    end
-    sum.to_f / self.checkins.size
-  end
-
-  # def sort_ch
-
-  # end
 
 end
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -29,18 +29,22 @@ class Brewery < ApplicationRecord
     self.photo.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default_brewery.png')), filename: 'default_brewery.png') unless self.photo.attached?
   end
 
-  def average_rating
-    sum = 0
-    self.checkins.each do |checkin|
-      sum += checkin.rating
-    end
-    sum.to_f / self.checkins.size
+  def average_rating # average of ratings counting zeros 
+    self.checkins
+      .average(:rating)
+    # .where("rating >= ?", 0) # in case I change table to str if no rating given
   end
 
-  def uniq_users
+  def uniq_users # counts num of unique users that have checked in a beer by brewery
+    self.checkin_authors.size
+  end
+
+  def checkins_this_month # num of checkins in current calendar month
+    dt = DateTime.now
+    bom = dt.beginning_of_month
+    eom = dt.end_of_month
     self.checkins
-      .group(:author_id)
-      .count
+      .where("checkins.created_at >= ? and checkins.created_at <= ?", bom, eom)
       .size
   end
 end
