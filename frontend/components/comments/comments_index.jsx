c0e2b894ae8c6d44c9b665dbcd209b2a1d5dc187
ccurnuck <calvin.curnuck@gmail.com>
2020-04-08 13:21:06 -0400

    Removed old commented code

--- frontend/components/comments/comments_index.jsx
+++ frontend/components/comments/comments_index.jsx
@@ -6,37 +6,6 @@ class CommentsIndex extends React.Component {
     super(props);
   }
 
-  // componentDidMount() {
-  //   this.setState({ commentIds: this.props.commentIds })
-  // }
-
-  // static getDerivedStateFromProps(nextProps, prevState){
-  //   // commentId vals are always unique
-  //   const nextPropArr = nextProps.checkin.commentIds;
-  //   const prevStateArr = prevState.checkin.commentIds;
-  //   const bool = nextPropArr.length === prevStateArr.length && nextPropArr.every(function(value, index) { 
-  //     return value === prevStateArr[index]
-  //   }); // compares array of ids to check if update is needed
-
-  //   if(!bool) {
-  //     return { checkin: nextProps.checkin }; 
-  //     // sets state to newly passed in checkin, forcing re-render
-  //   } else return null;
-  // }
-  
-  // tested without and still works but want to leave it in case it is needed
-  // componentDidUpdate(prevProps, prevState) {
-  //   const prevPropArr = prevProps.checkin.commentIds;
-  //   const prevStateArr = prevState.checkin.commentIds;
-  //   const bool = prevPropArr.length === prevStateArr.length && prevPropArr.every(function(value, index) { 
-  //     return value === prevStateArr[index]
-  //   });
-  //   if (!bool) {
-  //     //Perform some operation here
-  //     this.setState({ checkin: this.props.checkin });
-  //   }
-  // }
-
   render() {
     const commentIds = this.props.checkin.commentIds;
     
