a5ab839be186aaf86d3bc952a8150f4b5bead454
ccurnuck <calvin.curnuck@gmail.com>
2020-04-09 17:15:25 -0400

    Added Icons to Splash

--- frontend/components/splash/site_features.jsx
+++ frontend/components/splash/site_features.jsx
@@ -10,16 +10,19 @@ export default () => {
       <div className="items-container">
 
         <div className="site-info-item-1 feature-item">
+          <i className="fas fa-beer"></i>
           <h3 className="item-1-title feature-title">Discover</h3>
           <p className="item-1-body feature-body">Discover and try new beers!</p>
         </div>
 
         <div className="site-info-item-2 feature-item">
+          <i className="far fa-comments"></i>
           <h3 className="item-2-title feature-title">Review and Share</h3>
           <p className="item-2-body feature-body">See beers being reviewed on your feed and share your own reviews!</p>
         </div>
 
         <div className="site-info-item-3 feature-item">
+          <i className="far fa-thumbs-up"></i>
           <h3 className="item-3-title feature-title">Comment &amp; Toast</h3>
           <p className="item-3-body feature-body">Tell your friends what you think of beers they've had and give them a 'toast' from anywhere in the world!</p>
         </div>
